572832f102972d898b57972b06e31c41028a0ea4
tgdivy <me@divyb.xyz>
2022-11-04 23:47:26 +0200

    init frontend and backend

---
+++ api/src/index.py
@@ -0,0 +1,8 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/data", methods=["GET"])
+def hello_world():
+    return jsonify("Hello, World!")
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MapMain from "./3DMapVisualizer/MapMain";
+function App() {
+  return (
+    <div className="App">
+      <MapMain />
+    </div>
+  );
+}
+
+export default App;
