4c197a03413afdeda4f956208aeb8639d1e7b21a
tgdivy <me@divyb.xyz>
2022-11-05 17:42:05 +0200

    Ignore

--- src/spatial.py
+++ src/spatial.py
@@ -10,21 +10,43 @@ class OpenStreetMap:
         self.osm = OSM(get_data("london", directory=data_path))
 
     def get_data(self):
-        """_summary_
+        landuse = self.get_landuse_data()
+        # poi = self.get_point_of_interest_data()
 
-        Returns:
-            _type_: List(List(Float, Float))
-        """
-        landuse = self.osm.get_landuse()
+        return (landuse,)
+
+    def get_landuse_data(self):
+        landuse = self.osm.get_landuse(
+            custom_filter={"landuse": True, "lon": True, "lat": True}
+        )
+
+        # Drop all columns except for the longitude, latitude, and the type of landuse
+
+        # Drow any row that has at least one NaN value
+        # landuse.dropna(inplace=True)
         landuse.plot(column="landuse", legend=True, figsize=(10, 6))
         plt.show()
 
+        # landuse = landuse[["lon", "lat", "landuse"]]
+
         return landuse
+
+    def get_point_of_interest_data(self):
+        poi = self.osm.get_pois(
+            custom_filter={"amenity": True, "lon": True, "lat": True}
+        )
+        poi = poi[["lon", "lat", "amenity"]]
+        poi.dropna(inplace=True)
+
+        return poi
 
 
 if __name__ == "__main__":
     osm = OpenStreetMap("data/osm")
-    df = osm.get_data()
-    print(df.head())
-    print(df.columns)
-    print(df["tags"].unique())
+    dfs = osm.get_data()
+
+    for i, df in enumerate(dfs):
+        df.to_csv(f"data/osm/{i}.csv", index=False)
+    # print(df.head())
+    # print(df.columns)
+    # print(df["tags"].unique())
