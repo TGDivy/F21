425c6ada0899a684db652880ee036949212ec9bf
linh6si <linh.bui@6si.ai>
2022-11-05 21:30:20 +0200

    onehot

--- src/train_model.py
+++ src/train_model.py
@@ -6,19 +6,63 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import _pickle as cPickle
+from sklearn import preprocessing
 
 class DataLoader:
-    def __init__(self, data_source):  # array: data - time - features - has crimed - type_data
-        self.datasource = pd.read_csv(data_source)
+    def __init__(self):  # array: data - time - features - has crimed - type_data
+        osm_data = pd.read_csv("../data/osm_data_cell_onehot.csv")
+        columns_to_drop = (osm_data.sum(axis=0) > 1000)
+        columns_to_drop[:2] = True
+        print(osm_data.loc[:, -columns_to_drop])
+
+        osm_data = osm_data.loc[:,columns_to_drop]
+        uk_data = pd.read_csv("../data/uk_police_data/uk_police_data_cell.csv")
+        uk_police_data = pd.get_dummies(uk_data)
+        uk_police_data['crime_case'] = uk_police_data.iloc[:, 2:].sum(axis=1)
+        uk_police_data_sum = uk_police_data.loc[:, ["cell_id", "crime_case"]]
+        data_out = pd.merge(osm_data, uk_police_data_sum, on='cell_id', how='outer')
+        self.datasource = data_out.fillna(0)
+
+        x = self.datasource.values  # returns a numpy array
+        min_max_scaler = preprocessing.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(x)
+        self.datasource = pd.DataFrame(x_scaled)
+        self.datasource.to_csv('data.csv')
+
         self.datasource['is_train'] = np.random.uniform(0, 1, len(self.datasource)) <= 0.75
-        print(self.datasource)
-        self.y_name = np.array(['hotspot'])
+        self.y_name = np.array(['crime_case'])
 
     def load_data(self):
         train = self.datasource[self.datasource['is_train']==True]
         test = self.datasource[self.datasource['is_train']==False]
-        print(train[train.columns[1:8]],  train[train.columns[0]], test[test.columns[1:8]],  test[test.columns[0]])
-        return train[train.columns[1:8]],  train[train.columns[0]], test[test.columns[1:8]],  test[test.columns[0]]
+        return train[train.columns[2:-1]],  train[train.columns[-1]], test[test.columns[2:-1]],  test[test.columns[-1]]
+
+
+def main():
+    X_train, y_train, X_test, y_test = DataLoader().load_data()
+    # 1
+    model_BernoulliNB = naive_bayes.BernoulliNB()
+    model_BernoulliNB.fit(X_train, y_train)
+    predicted_y = model_BernoulliNB.predict(X_test)
+    print(metrics.classification_report(y_test, predicted_y))
+    with open('./model/model_BernoulliNB.pkl', 'wb') as fid:
+        cPickle.dump(model_BernoulliNB, fid)
+    # # 2
+    # model_MultinomialNB = naive_bayes.MultinomialNB()
+    # model_MultinomialNB.fit(X_train, y_train)
+    # predicted_y_MultinomialNB = model_MultinomialNB.predict(X_test)
+    # print(metrics.classification_report(y_test, predicted_y_MultinomialNB))
+    # with open('./model/model_MultinomialNB.pkl', 'wb') as fid:
+    #     cPickle.dump(model_MultinomialNB, fid)
+
+    # load model:
+    # with open('my_dumped_classifier.pkl', 'rb') as fid:
+    #     gnb_loaded = cPickle.load(fid)
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 # class LSTMClassification(nn.Module):
@@ -76,28 +120,3 @@ class DataLoader:
 #         accuracy = np.sum(predicts == labels) / len(self.test_loader)
 #         print(accuracy)
 
-
-def main():
-    X_train, y_train, X_test, y_test = DataLoader("../data/sampletrain.csv").load_data()
-    # 1
-    model_BernoulliNB = naive_bayes.BernoulliNB()
-    model_BernoulliNB.fit(X_train, y_train)
-    predicted_y = model_BernoulliNB.predict(X_test)
-    print(metrics.classification_report(y_test, predicted_y))
-    with open('./model/model_BernoulliNB.pkl', 'wb') as fid:
-        cPickle.dump(model_BernoulliNB, fid)
-    # 2
-    model_MultinomialNB = naive_bayes.MultinomialNB()
-    model_MultinomialNB.fit(X_train, y_train)
-    predicted_y_MultinomialNB = model_MultinomialNB.predict(X_test)
-    print(metrics.classification_report(y_test, predicted_y_MultinomialNB))
-    with open('./model/model_MultinomialNB.pkl', 'wb') as fid:
-        cPickle.dump(model_MultinomialNB, fid)
-
-    # load model:
-    # with open('my_dumped_classifier.pkl', 'rb') as fid:
-    #     gnb_loaded = cPickle.load(fid)
-
-
-if __name__ == "__main__":
-    main()
